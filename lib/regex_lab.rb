52a2bd0be7eeac7675e082604059428893264e56
Kyle Luke <th.oldschool@gmail.com>
2019-08-25 19:53:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  !word.match(/\b[AEIOUaeiou][a-z]*\b/).nil? ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[un][a-z][ing]*\b/)
 end
 
 def words_five_letters_long(text)
