ab34bd9b13d8b4c8858268179fa2709c1d3c39fa
Kyle Luke <th.oldschool@gmail.com>
2019-08-25 20:33:21 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !word.match(/\b[AEIOUaeiou][a-z]*\b/).nil? ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  p text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !text.match(/\b[A-Z]+\w+[.,"!?]/).nil? ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  p phone.match(/^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
 end
