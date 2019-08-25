3b7c7205ca86537ad8634498a4db5390a06ef8ff
Kyle Luke <th.oldschool@gmail.com>
2019-08-25 20:02:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.scan(/\b(un)[a-z]*(ing)\b/)
+  p text if text.scan(/\b(un)[a-z]*(ing)\b/)
     p text 
   end
 end
